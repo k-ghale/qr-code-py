c93492e0d01920e6ef29aa8bf33aea02be4e4301
k-ghale <kabinghale888@gmail.com>
2026-03-03 20:43:21 -0500

    closed: finished the project

--- qr-code.py
+++ qr-code.py
@@ -4,10 +4,8 @@ import qrcode
 website_lisk = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 
 qr = qrcode.QRCode(version=1, box_size=5, border=5)
-
 qr.add_data(website_lisk)
 qr.make()
 
-img = qr.make_image(fill_color = "black", back_color="white")
-
-img.save('youtube_qr.png')
+img = qr.make_image(fill_color="black", back_color="white")
+img.save('youtube_img_2.png')
